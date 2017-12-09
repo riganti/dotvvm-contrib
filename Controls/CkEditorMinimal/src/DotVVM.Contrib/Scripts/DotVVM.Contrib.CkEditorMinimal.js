79b3954043aff2d06d0206a1afbcbad3a656221e
Tomas Herceg <tomas.herceg@riganti.cz>
2017-12-09 17:41:53 +0100

    CkEditorMinimal - control now responds to viewmodel changes correctly

--- Controls/CkEditorMinimal/src/DotVVM.Contrib/Scripts/DotVVM.Contrib.CkEditorMinimal.js
+++ Controls/CkEditorMinimal/src/DotVVM.Contrib/Scripts/DotVVM.Contrib.CkEditorMinimal.js
@@ -1,24 +1,49 @@
-﻿
-function setupCkEditor(id, prop) {
-    var editor = CKEDITOR.instances[id];
-    var htmlText = ko.isObservable(prop.html) ? prop.html() : prop.html;
-
-    if (editor) {
-        editor.destroy(true);
-    }
-
-    CKEDITOR.replace(id);
-    CKEDITOR.instances[id].setData(htmlText);
-    CKEDITOR.instances[id].on('change', function () { prop.html(CKEDITOR.instances[id].getData()); });
-}
-
-ko.bindingHandlers["dotvvm-contrib-CkEditorMinimal"] = { 
-    init: function (element, valueAccessor, allBindingsAccessor, viewModel, bindingContext) {
-        var prop = ko.unwrap(valueAccessor());
-        if (!element.id){
-            element.setAttribute("id", element.attributes["data-dotvvm-id"].value);
+﻿(function() {
+    function setupCkEditor(id, prop, element) {
+        var editor = CKEDITOR.instances[id];
+        if (editor) {
+            editor.destroy(true);
         }
 
-        setupCkEditor(element.id, prop);
+        CKEDITOR.replace(id);
+        CKEDITOR.instances[id].on('change',
+            function() {
+                if (ko.isWritableObservable(prop.html)) {
+                    element.isUpdating = true;
+                    prop.html(CKEDITOR.instances[id].getData());
+                    element.isUpdating = false;
+                }
+            });
     }
-};
+
+    function setCkEditorText(id, prop) {
+        var htmlText = ko.isObservable(prop.html) ? prop.html() : prop.html;
+        CKEDITOR.instances[id].setData(htmlText);
+    }
+
+    ko.bindingHandlers["dotvvm-contrib-CkEditorMinimal"] = {
+        init: function(element, valueAccessor, allBindingsAccessor, viewModel, bindingContext) {
+            var prop = ko.unwrap(valueAccessor());
+            if (!element.id) {
+                element.setAttribute("id", element.attributes["data-dotvvm-id"].value);
+            }
+
+            setupCkEditor(element.id, prop, element);
+
+            if (ko.isObservable(prop.html)) {
+                prop.html.subscribe(function () {
+                    if (!element.isUpdating) {
+                        setCkEditorText(element.id, prop);
+                    }
+                });
+            }
+        },
+        update: function(element, valueAccessor, allBindingsAccessor, viewModel, bindingContext) {
+            var prop = ko.unwrap(valueAccessor());
+            if (!element.isUpdating) {
+                setCkEditorText(element.id, prop);
+            }
+        }
+    };
+
+})();
