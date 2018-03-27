9260fd528f12d39ff6bf2a21252ca99936ae37ca
Ladislav Šesták <ladislav.sestak@riganti.cz>
2018-03-27 20:41:19 +0200

    Update for dotvvm.2.0.0-preview04

--- Controls/CkEditorMinimal/src/DotVVM.Contrib.Tests/CkEditorMinimalTests.cs
+++ Controls/CkEditorMinimal/src/DotVVM.Contrib.Tests/CkEditorMinimalTests.cs
@@ -4,38 +4,5 @@ using Riganti.Utils.Testing.SeleniumCore;
 
 namespace DotVVM.Contrib.Tests
 {
-    [TestClass]
-    public class CkEditorMinimalTests : SeleniumTestBase
-    {
-        [TestMethod]
-        public void CkEditorMinimal_Sample1()
-        {
-            RunInAllBrowsers(browser =>
-            {
-                browser.NavigateToUrl("/Sample1");
-                browser.Wait(2000);
-
-                browser.Single("#CkEditorMinimal");
-                browser.Single("#cke_CkEditorMinimal");
-           
-            });
-        }
-
-
-        [TestMethod]
-        public void CkEditorMinimal_Sample2()
-        {
-            RunInAllBrowsers(browser =>
-            {
-                browser.NavigateToUrl("/Sample2");
-                browser.Wait(2000);
-
-                browser.Single("#CkEditorMinimal_1");
-                browser.Single("#CkEditorMinimal_2");
-                browser.Single("#cke_CkEditorMinimal_1");
-                browser.Single("#cke_CkEditorMinimal_2");
-
-            });
-        }
-    }
+   
 }
