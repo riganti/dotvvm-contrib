58810e460aebaff2d41b1e4dd3c9c138e99286e9
ppech <2861692+ppech@users.noreply.github.com>
2019-01-14 15:22:58 +0100

    test: add basic test

--- Controls/BootstrapDatepicker/src/DotVVM.Contrib.Tests/ComponentTests.cs
+++ Controls/BootstrapDatepicker/src/DotVVM.Contrib.Tests/ComponentTests.cs
@@ -1,5 +1,5 @@
-using System;
 using DotVVM.Contrib.Tests.Core;
+using OpenQA.Selenium;
 using Xunit;
 using Xunit.Abstractions;
 
@@ -12,12 +12,29 @@ namespace DotVVM.Contrib.Tests
         {
             RunInAllBrowsers(browser =>
             {
-                browser.NavigateToUrl();
+                browser.NavigateToUrl("/Sample2");
+                browser.Wait(2000);
+
+                var dp1 = browser.Single("dp1", By.Id);
+                Assert.Equal(string.Empty, dp1.GetValue());
+
+                dp1.Clear();
+                dp1.SendKeys("13.1.2019");
+                dp1.SendEnterKey();
+
+                Assert.Equal("13.01.2019", dp1.GetValue());
+
+                var dp2 = browser.Single("dp2", By.Id);
+                Assert.Equal("01/13/2019", dp2.GetValue());
+
+                var dp3 = browser.Single("dp3", By.Id);
+                Assert.Equal("13/01/2019", dp3.GetValue());
+
             });
         }
 
         public ComponentTests(ITestOutputHelper output) : base(output)
         {
         }
-    } 
+    }
 }
