cd4e4ab00ae2863e9e2ca0aca070413d2e0d7b66
Tomáš Herceg <tomas.herceg@riganti.cz>
2019-02-10 15:00:35 +0100

    Bootstrap Colorpicker control implemented

---
+++ Controls/BootstrapColorpicker/src/DotVVM.Contrib/Scripts/DotVVM.Contrib.BootstrapColorpicker.js
@@ -0,0 +1,22 @@
+﻿ko.bindingHandlers["dotvvm-contrib-BootstrapColorpicker"] = {
+    init: function (element, valueAccessor, allBindingsAccessor, viewModel, bindingContext) {
+
+        $(element)
+            .colorpicker({
+                format: "hex"
+            })
+            .on("colorpickerChange", function(event) {
+                var property = valueAccessor();
+                if (ko.isWritableObservable(property)) {
+                    property(event.color.toHexString());
+                }
+            });
+
+    },
+    update: function (element, valueAccessor, allBindingsAccessor, viewModel, bindingContext) {
+        var value = ko.unwrap(valueAccessor());
+
+        $(element)
+            .colorpicker("setValue", value);
+    }
+};
