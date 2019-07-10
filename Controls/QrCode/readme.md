0f609b6decd508671fb5d8f3b5a65970f4c00b6f
PadreSVK <patrik.svikruha@gmail.com>
2019-07-10 14:31:43 +0200

    init

---
+++ Controls/QrCode/readme.md
@@ -0,0 +1,12 @@
+# QrCode
+
+Controls render canvas with qrcode based od Url property.
+
+## Sample 1: Something
+
+You can do this:
+
+```DOTHTML
+<dc:QrCode Url="{value: Url}"></dc:QrCode>
+<dc:QrCode Url="https://www.google.com/"></dc:QrCode>
+```
---
+++ Controls/QrCode/src/DotVVM.Contrib/QrCode.cs
@@ -0,0 +1,45 @@
+﻿using DotVVM.Framework.Controls;
+using DotVVM.Framework.Hosting;
+using DotVVM.Framework.Binding;
+
+namespace DotVVM.Contrib
+{
+    /// <summary>
+    /// Renders a QrCode into canvas
+    /// </summary>
+    public class QrCode : HtmlGenericControl
+    {
+        public static readonly DotvvmProperty UrlProperty
+           = DotvvmProperty.Register<string, QrCode>(c => c.Url, null);
+
+
+        public QrCode() : base("div")
+        {
+        }
+
+        [MarkupOptions(Required = true)]
+        public string Url
+        {
+            get => (string)GetValue(UrlProperty);
+            set => SetValue(UrlProperty, value);
+        }
+
+        protected override void OnPreRender(IDotvvmRequestContext context)
+        {
+            context.ResourceManager.AddRequiredResource("dotvvm.contrib.QrCode");
+            base.OnPreRender(context);
+        }
+        protected override void AddAttributesToRender(IHtmlWriter writer, IDotvvmRequestContext context)
+        {
+            writer.AddKnockoutDataBind("dotvvm-contrib-QrCode", GetControlBinding());
+            base.AddAttributesToRender(writer, context);
+        }
+        private KnockoutBindingGroup GetControlBinding()
+        {
+            return new KnockoutBindingGroup
+            {
+                {"url", this, UrlProperty}
+            };
+        }
+    }
+}
