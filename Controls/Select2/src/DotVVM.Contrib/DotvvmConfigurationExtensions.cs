1018ab7b1c77915a1459f1a8d6058646cb0751bc
Michal Tichý <michal.tichy@riganti.cz>
2019-08-07 10:09:08 +0200

    Select2 - fixed resource registration

--- Controls/Select2/src/DotVVM.Contrib/DotvvmConfigurationExtensions.cs
+++ Controls/Select2/src/DotVVM.Contrib/DotvvmConfigurationExtensions.cs
@@ -10,28 +10,29 @@ namespace DotVVM.Contrib
         public static void AddContribSelect2Configuration(this DotvvmConfiguration config)
         {
             // register tag prefix
+            var assemblyName = typeof(Select2.Select2).Assembly.GetName().Name;
             config.Markup.Controls.Add(new DotvvmControlConfiguration()
             {
-                Assembly = "DotVVM.Contrib.Select2",
-                Namespace = "DotVVM.Contrib",
+                Assembly = assemblyName,
+                Namespace = assemblyName,
                 TagPrefix = "dc"
             });
 
             // register additional resources for the control and set up dependencies
             config.Resources.Register("select2", new ScriptResource()
             {
-                Location = new EmbeddedResourceLocation(typeof(Select2.Select2).GetTypeInfo().Assembly, "DotVVM.Contrib.Scripts.select2.select2.min.js"),
-                Dependencies = new[] { "dotvvm", "select2.css", "jquery" }
+                Location = new EmbeddedResourceLocation(typeof(Select2.Select2).GetTypeInfo().Assembly, $"{assemblyName}.Scripts.select2.select2.min.js"),
+                Dependencies = new[] { "dotvvm", "dotvvm.contrib.select2.css", "jquery" }
             });
 
-            config.Resources.Register("select2.css", new StylesheetResource()
+            config.Resources.Register("dotvvm.contrib.select2.css", new StylesheetResource()
             {
-                Location = new EmbeddedResourceLocation(typeof(Select2.Select2).GetTypeInfo().Assembly, "DotVVM.Contrib.Scripts.select2.select2.min.css")
+                Location = new EmbeddedResourceLocation(typeof(Select2.Select2).GetTypeInfo().Assembly, $"{assemblyName}.Styles.select2.select2.min.css")
             });
 
             config.Resources.Register("dotvvm.contrib.Select2", new ScriptResource()
             {
-                Location = new EmbeddedResourceLocation(typeof(Select2.Select2).GetTypeInfo().Assembly, "DotVVM.Contrib.Scripts.DotVVM.Contrib.Select2.js"),
+                Location = new EmbeddedResourceLocation(typeof(Select2.Select2).GetTypeInfo().Assembly, $"{assemblyName}.Scripts.DotVVM.Contrib.Select2.js"),
                 Dependencies = new[] { "select2" }
             });
         }
