c8b6bbb13263486087202315f88ce71ced751966
Michal Mrnuštík <michal.mrnustik@riganti.cz>
2021-05-19 16:17:54 +0200

    Add support for computed icon values

--- Controls/FAIcon/src/DotVVM.Contrib/Scripts/DotVVM.Contrib.FAIcon.js
+++ Controls/FAIcon/src/DotVVM.Contrib/Scripts/DotVVM.Contrib.FAIcon.js
@@ -1,27 +1,28 @@
 ﻿ko.bindingHandlers["dotvvm-contrib-FAIcon"] = {
     init: function (element, valueAccessor, allBindingsAccessor, viewModel, bindingContext) {
-
-        valueAccessor().subscribe(function (previousValue) {
-            const splitterIndex = previousValue.lastIndexOf("_");
-            const previousIconValue = previousValue.substr(0, splitterIndex);
-            const previousIconValueClass = "fa-" + previousIconValue.replace('_', '-')
-            const previousStyle = previousValue.substr(splitterIndex + 1);
-            const previousStyleClass = `fa${previousStyle.charAt(0)}`;
-            element.classList.remove(previousStyleClass)
-            element.classList.remove(previousIconValueClass);
-        }, this, "beforeChange");
     },
     update: function (element, valueAccessor, allBindingsAccessor, viewModel, bindingContext) {
+        removePreviousClasses(element);
         const value = ko.unwrap(valueAccessor());
+        addClassesBasedOnIcon(element, value);
 
-        const splitterIndex = value.lastIndexOf("_");
-        const iconValue = value.substr(0, splitterIndex);
-        const iconValueClass = "fa-" + iconValue.replace('_', '-')
-        const style = value.substr(splitterIndex + 1);
-        const styleClass = `fa${style.charAt(0)}`;
-        
-        element.classList.add(styleClass);
-        element.classList.add(iconValueClass);
-    }
+        function removePreviousClasses(element) {
+            const classListArray = [...element.classList];
+            const previousFontAwesomeClasses = classListArray.filter(function (value) {
+                return value.startsWith("fa");
+            })
+            element.classList.remove(...previousFontAwesomeClasses);
+        }
+
+        function addClassesBasedOnIcon(element, value) {
+            const splitterIndex = value.lastIndexOf("_");
+            const iconValue = value.substr(0, splitterIndex);
+            const iconClass = "fa-" + iconValue.replace('_', '-')
+            const style = value.substr(splitterIndex + 1);
+            const styleClass = `fa${style.charAt(0)}`;
+
+            element.classList.add(styleClass);
+            element.classList.add(iconClass);
+        }
+    },
 };
-
