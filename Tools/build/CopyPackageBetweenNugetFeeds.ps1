c786690a9d414c2eca4046de17ad4e1639cd75ef
Tomáš Herceg <tomas.herceg@riganti.cz>
2021-06-29 09:50:52 +0200

    Batch publish support added

--- Tools/build/CopyPackageBetweenNugetFeeds.ps1
+++ Tools/build/CopyPackageBetweenNugetFeeds.ps1
@@ -3,15 +3,21 @@ Param(
     [string]$server, 
     [string]$internalServer, 
     [string]$apiKey,
-    [string]$packageId
+    [string]$packageIds
 )
 
-    & .\tools\nuget.exe install $packageId -OutputDirectory .\tools\packages -version $version -DirectDownload -NoCache -DependencyVersion Ignore -source $internalServer
-    $nupkgFile = dir -s ./tools/packages/$packageId.$version.nupkg | Select -First 1
-    Write-Host "Downloaded package located on '$nupkgFile'" 
-    
-    Write-Host "Uploading package..."
-    & .\Tools\nuget.exe push $nupkgFile -source $server -apiKey $apiKey
-    Write-Host "Package uploaded to $server."
+foreach ($packageId in $packageIds.Split(',')) { 
 
-    Remove-Item $nupkgFile
+	$packageId = $packageId.Trim()
+
+	& .\tools\nuget.exe install $packageId -OutputDirectory .\tools\packages -version $version -DirectDownload -NoCache -DependencyVersion Ignore -source $internalServer
+	$nupkgFile = dir -s ./tools/packages/$packageId.$version.nupkg | Select -First 1
+	Write-Host "Downloaded package located on '$nupkgFile'" 
+
+	Write-Host "Uploading package..."
+	& .\Tools\nuget.exe push $nupkgFile -source $server -apiKey $apiKey
+	Write-Host "Package uploaded to $server."
+
+	Remove-Item $nupkgFile
+
+}
