f83b4cce13a8fefae3990505f21d7c1ab8747f76
Tomáš Herceg <tomas.herceg@riganti.cz>
2022-02-25 09:39:08 +0100

    CookieBar - test project updated

--- Controls/CookieBar/src/DotVVM.Contrib.Samples/Startup.cs
+++ Controls/CookieBar/src/DotVVM.Contrib.Samples/Startup.cs
@@ -20,7 +20,7 @@ namespace DotVVM.Contrib.Samples
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             // use DotVVM
             var dotvvmConfiguration = app.UseDotVVM<DotvvmStartup>(env.ContentRootPath);
