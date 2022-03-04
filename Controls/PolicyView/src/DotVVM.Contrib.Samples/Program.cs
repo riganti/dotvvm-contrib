150a400dec3c6445ecdbe71278454d563a0eec4f
Radek Čížkovský <radek.cizkovsky@riganti.cz>
2022-03-04 21:42:28 +0100

    PolicyView - updated to latest FW

--- Controls/PolicyView/src/DotVVM.Contrib.Samples/Program.cs
+++ Controls/PolicyView/src/DotVVM.Contrib.Samples/Program.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace DotVVM.Contrib.Samples
 {
@@ -11,14 +13,16 @@ namespace DotVVM.Contrib.Samples
     {
         public static void Main(string[] args)
         {
-            var host = new WebHostBuilder()
-                .UseKestrel()
-                .UseContentRoot(Directory.GetCurrentDirectory())
-                .UseIISIntegration()
-                .UseStartup<Startup>()
-                .Build();
-
-            host.Run();
+            BuildWebHost(args).Run();
         }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .ConfigureLogging((context, builder) =>
+                {
+                    builder.AddConsole();
+                })
+                .Build();
     }
 }
