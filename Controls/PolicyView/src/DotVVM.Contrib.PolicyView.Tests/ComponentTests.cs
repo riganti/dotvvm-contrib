4110e3604c789d71556ea0ae8e0d752b9fa4d8a2
František Siladi <frantisek.siladi@riganti.cz>
2022-03-18 07:50:15 +0100

    Add PolicyView tests

--- Controls/PolicyView/src/DotVVM.Contrib.PolicyView.Tests/ComponentTests.cs
+++ Controls/PolicyView/src/DotVVM.Contrib.PolicyView.Tests/ComponentTests.cs
@@ -1,4 +1,6 @@
 using DotVVM.Contrib.PolicyView.Tests.Core;
+using OpenQA.Selenium;
+using Riganti.Selenium.Core;
 using Xunit;
 using Xunit.Abstractions;
 
@@ -11,7 +13,56 @@ namespace DotVVM.Contrib.PolicyView.Tests
         {
             RunInAllBrowsers(browser =>
             {
-                browser.NavigateToUrl();
+                browser.NavigateToUrl("/Sample1");
+                var body = browser.Single("body", By.TagName);
+
+                var signBtn = browser.Single("#sign-in-button");
+
+                AssertUI.NotContainsElement(body, "#in-role-requirements-met");
+                AssertUI.NotContainsElement(body, "#in-role-requirements-not-met");
+
+                AssertUI.NotContainsElement(body, "#autenticated-req-met");
+                AssertUI.ContainsElement(body, "#autenticated-req-not-met");
+
+                AssertUI.NotContainsElement(body, "#combinated-req-met");
+                AssertUI.NotContainsElement(body, "#combinated-req-not-met");
+
+                signBtn.Click();
+                body = browser.Single("body", By.TagName);
+
+                AssertUI.NotContainsElement(body, "#in-role-req-met");
+                AssertUI.ContainsElement(body, "#in-role-req-not-met");
+
+                AssertUI.ContainsElement(body, "#autenticated-req-met");
+                AssertUI.NotContainsElement(body, "#autenticated-req-not-met");
+
+                AssertUI.ContainsElement(body, "#combinated-req-met");
+                AssertUI.NotContainsElement(body, "#combinated-req-not-met");
+
+                browser.Single("#sign-role-button").Click();
+                body = browser.Single("body", By.TagName);
+
+                AssertUI.ContainsElement(body, "#in-role-req-met");
+                AssertUI.NotContainsElement(body, "#in-role-req-not-met");
+
+                AssertUI.ContainsElement(body, "#autenticated-req-met");
+                AssertUI.NotContainsElement(body, "#autenticated-req-not-met");
+
+                AssertUI.ContainsElement(body, "#combinated-req-met");
+                AssertUI.NotContainsElement(body, "#combinated-req-not-met");
+
+                browser.Single("#sign-out-button").Click();
+                body = browser.Single("body", By.TagName);
+
+                AssertUI.NotContainsElement(body, "#in-role-requirements-met");
+                AssertUI.NotContainsElement(body, "#in-role-requirements-not-met");
+
+                AssertUI.NotContainsElement(body, "#autenticated-req-met");
+                AssertUI.ContainsElement(body, "#autenticated-req-not-met");
+
+                AssertUI.NotContainsElement(body, "#combinated-req-met");
+                AssertUI.NotContainsElement(body, "#combinated-req-not-met");
+
             });
         }
 
