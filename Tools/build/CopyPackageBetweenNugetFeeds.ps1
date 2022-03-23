4f30a62c32d19b835c023cee71a6bf967fb12367
Tomáš Herceg <tomas.herceg@riganti.cz>
2022-03-23 11:10:51 +0100

    Updated copy to official NuGet script

--- Tools/build/CopyPackageBetweenNugetFeeds.ps1
+++ Tools/build/CopyPackageBetweenNugetFeeds.ps1
@@ -3,12 +3,30 @@ Param(
     [string]$server, 
     [string]$internalServer, 
     [string]$apiKey,
-    [string]$packageIds
+    [string]$controlName
 )
 
-foreach ($packageId in $packageIds.Split(',')) { 
+### Configuration
 
-	$packageId = "DotVVM.Contrib." + $packageId.Trim()
+$packages = @()
+
+if ($controlName -eq "") {
+	# add all controls 
+	$controlDirs = Get-ChildItem Controls/ -Directory | Where-Object { $_.Name.StartsWith("_") -eq $false } | Select-Object Name
+	foreach ($controlDir in $controlDirs) {
+		$packages += @([pscustomobject]@{ Package = "DotVVM.Contrib.$($controlDir.Name)"; Directory = "Controls\$($controlDir.Name)\src\DotVVM.Contrib.$($controlDir.Name)" })
+	}
+} else {
+	# split comma-separated controls
+	foreach ($controlDir in $controlName.Split(',')) {
+		$controlDir = $controlDir.Trim()
+		$packages += @([pscustomobject]@{ Package = "DotVVM.Contrib." + $controlDir; Directory = "Controls\" + $controlDir + "\src\DotVVM.Contrib." + $controlDir })
+	}
+}
+
+foreach ($package in $packages) { 
+
+	$packageId = $package.Package
 
 	& .\tools\nuget.exe install $packageId -OutputDirectory .\tools\packages -version $version -DirectDownload -NoCache -DependencyVersion Ignore -source $internalServer
 	$nupkgFile = dir -s ./tools/packages/$packageId.$version.nupkg | Select -First 1
